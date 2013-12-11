f0f5be37cf8a0ba8f07cb3f8e55e88590cbfca59
SM <sol@archetyped.com>
2013-12-10 16:25:46 -1000

    Update dev version

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: Eyes Only
 Plugin URI: http://archetyped.com/tools/eyes-only/
 Description: The ultimate tool for publishing your private thoughts in public
-Version: 1.0.5
+Version: dev (BETA)
 Author: Archetyped
 Author URI: http://archetyped.com
 */
